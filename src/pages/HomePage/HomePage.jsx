0b6e33bcf5c3609032d3f7a6c2ccfb325ab74779
tetiana-batura <taniaknu20@gmail.com>
2024-10-20 21:39:30 -0400

    remove unnecessary files, add hero section and colors to variables

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,12 +9,18 @@ function HomePage() {
   const [searchParams] = useSearchParams();
   const apiURL = `${import.meta.env.VITE_BACKEND_URL}/recipes`;
 
-  console.log(searchParams)
+  console.log(searchParams);
 
   // if no params, default recipes
   // if params come from form submit, use them on a backend
 
-  return <div>HomePage</div>;
+  return (
+  <header className="header">
+    <h2 className="header__subtitle" >RECIPIE FINDER</h2>
+<h1 className="header__title">TASTE MATCH</h1>
+<h3 className="header__slogan">Discover Recipes Tailored to You</h3>
+  </header>
+  );
 }
 
 export default HomePage;
